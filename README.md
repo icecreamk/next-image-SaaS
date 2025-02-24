7b8357b6d8b97f4043fe239707afc527bc968378
fflinkexin <fflinkexin@chinatelecom.cn>
2025-02-24 16:48:56 +0800

    drizzle-zod

--- README.md
+++ README.md
@@ -33,3 +33,4 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 ### api
 - zod
+- drizzle-zod
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,21 +1,17 @@
-import { request } from "http";
+import { createUserSchema } from "@/server/db/validate-schema";
 import { NextRequest, NextResponse } from "next/server";
-import z from "zod";
 
 export function GET(request: NextRequest) {
-  const inputSchema = z.object({
-    name: z.string().max(10).min(3),
-    email: z.string().max(10).min(3),
-  });
-
   const query = request.nextUrl.searchParams;
   const name = query.get("name");
   const email = query.get("email");
+  const id = query.get("id");
 
   try {
-    const result = inputSchema.parse({
+    const result = createUserSchema.parse({
       name,
       email,
+      id
     });
     return NextResponse.json(result);
   } catch (error: any) {
